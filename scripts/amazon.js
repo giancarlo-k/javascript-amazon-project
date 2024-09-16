ad08248380ca158c43484aed34690c7adcc2feb1
giancarlo-k <giancarlokite@yahoo.com>
2024-09-16 12:54:25 -0400

    idk

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+const cart = [];
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -24,8 +26,8 @@ products.forEach((product) => {
         $${(product.priceCents / 100).toFixed(2)}
       </div>
 
-      <div class="product-quantity-container">
-        <select>
+      <div>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -60,6 +62,8 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
+      const quantity = Number(quantitySelector.value)
 
       let matchingItem;
 
@@ -70,11 +74,11 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
       } else {
         cart.push({
-          productId: productId,
-          quantity: 1
+          productId,
+          quantity
         });
       }
 
