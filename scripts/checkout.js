5435fa25ca3b0e6ad6b893b3fb826cd9b94c0fc7
giancarlo-k <giancarlokite@yahoo.com>
2024-09-19 16:36:41 -0400

    generate html for delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -24,49 +24,66 @@ cart.forEach((cartItem) => {
     }
   });
 
-    cartSummaryHTML += `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
-        <div class="delivery-date">
-          Delivery date: Tuesday, June 21
+  const deliveryOptionId = cartItem.deliveryOptionId;
+
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  const today = dayjs();
+  const deliveryDate = today.add(
+    deliveryOption.deliveryDays, 
+    'days'
+  );
+  const dateString = deliveryDate.format('dddd, MMMM D');
+
+  cartSummaryHTML += `
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="delivery-date">
+        Delivery date: ${dateString}
+      </div>
+
+      <div class="cart-item-details-grid">
+        <img class="product-image"
+          src="${matchingProduct.image}">
+
+        <div class="cart-item-details">
+          <div class="product-name">
+            ${matchingProduct.name}"
+          </div>
+          <div class="product-price">
+            $${formatCurrency(matchingProduct.priceCents)}
+          </div>
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary">
+              Update
+            </span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
+          </div>
         </div>
 
-        <div class="cart-item-details-grid">
-          <img class="product-image"
-            src="${matchingProduct.image}">
-
-          <div class="cart-item-details">
-            <div class="product-name">
-              ${matchingProduct.name}"
-            </div>
-            <div class="product-price">
-              $${formatCurrency(matchingProduct.priceCents)}
-            </div>
-            <div class="product-quantity">
-              <span>
-                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-              </span>
-              <span class="update-quantity-link link-primary">
-                Update
-              </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                Delete
-              </span>
-            </div>
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
           </div>
-
-          <div class="delivery-options">
-            <div class="delivery-options-title">
-              Choose a delivery option:
-            </div>
-            ${deliveryOptionsHTML(matchingProduct)}
-          </div>
+          ${deliveryOptionsHTML(matchingProduct, cartItem)}
         </div>
       </div>
-    `
+    </div>
+  `
 });
 
 
-function deliveryOptionsHTML(matchingProduct) {
+function deliveryOptionsHTML(matchingProduct, cartItem) {
   let html;
   deliveryOptions.forEach((deliveryOption) => {
     const today = dayjs();
@@ -80,9 +97,13 @@ function deliveryOptionsHTML(matchingProduct) {
     === 0 
       ? 'FREE'
       : `$${formatCurrency(deliveryOption.priceCents)} -`;
+
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
     html += `
         <div class="delivery-option">
           <input type="radio"
+            ${isChecked ? 'checked' : ''}
             class="delivery-option-input"
             name="delivery-option-${matchingProduct.id}">
           <div>
@@ -90,7 +111,7 @@ function deliveryOptionsHTML(matchingProduct) {
               ${dateString}
             </div>
             <div class="delivery-option-price">
-              ${priceString} - Shipping
+              ${priceString} Shipping
             </div>
           </div>
         </div>
